74874119f487d8265667d5a8153eab1f14c98086
Preetham B <bpreetham58@gmail.com>
2025-03-21 10:25:09 +0530

    login-signup-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-=======
-import AppRouter from "./Router"
->>>>>>> e3dc7fc7a4e4cb94d72b2b6c23a69fcc11bcc337
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -20,12 +15,5 @@ const App = () => {
     </Router>
   );
 };
-=======
-    <>
-      <AppRouter />
-    </>
-  )
-}
->>>>>>> e3dc7fc7a4e4cb94d72b2b6c23a69fcc11bcc337
 
 export default App;
